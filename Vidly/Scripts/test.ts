f286373a8bb60df4b8a6fc1696d3f8570a585f7c
likaihe <1047561425@qq.com>
2016-09-09 21:47:06 +1200

    demonstrate typescript

---
+++ Vidly/Scripts/test.ts
@@ -0,0 +1,34 @@
+﻿
+class movieShow {
+    src: string;  
+    
+    constructor(public name) {
+        if (name == "SuicideSquad")
+            this.src = "https://cdn.eventcinemas.co.nz/cdn/resources/movies/9620/images/skin.jpg";
+        if (name == "BadMom")
+            this.src = "https://cdn.eventcinemas.co.nz/cdn/resources/movies/9616/images/skin.jpg";  
+        if (name == "SausageParty")
+            this.src = "https://cdn.eventcinemas.co.nz/cdn/resources/movies/9640/images/skin.jpg";
+    }
+
+}
+
+
+
+function display(Movie: movieShow) {
+    document.getElementById("movieimg").setAttribute("src", Movie.src);
+}
+
+function removedemo() {
+    document.getElementById("demo").remove();
+}
+
+
+
+function Showimg(value) {
+    var movie = new movieShow(value);
+    display(movie);
+    removedemo();
+}
+
+
