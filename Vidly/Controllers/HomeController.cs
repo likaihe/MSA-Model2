3fd6d47f6b77194883ff0104f64f73febacf7470
likaihe <1047561425@qq.com>
2016-10-12 10:35:02 +1300

    pass the move model to the homepage

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         public ActionResult Index()
         {
-            return View();
+            var movielist = _context.Movies.Include(c => c.Genre).ToList();
+            return View(movielist);
         }
 
         public ActionResult About()
